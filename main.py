8be935e4db77821afa102f65a3ca23bde76db539
jdeljunco23 <jdeljuncoobregon@gmail.com>
2025-05-12 22:17:22 +0200

    cambios en uso de dataset para el preprocesamiento

--- main.py
+++ main.py
@@ -9,16 +9,16 @@ from metrics_evaluation import evaluate_all
 def process_batch(input_dir, output_dir):
     for root, _, files in os.walk(input_dir):
         for filename in files:
-            if filename.endswith('.png') or filename.endswith('.jpg'):
+            if (filename.endswith('.png') or filename.endswith('.jpg')) and '_mask' not in filename :
                 img_path = os.path.join(root, filename)
                 print(f"Procesando: {img_path}")
                 process_image(img_path, output_dir)
 
 if __name__ == "__main__":
-    test_dir = "Dataset_BUSI_with_GT"
+    test_dir = "Dataset_BUSI_train"
     output_dir = "preprocessed"
 
-    mode = "evaluate"
+    mode = "classic"
 
     if mode == "classic":
         process_batch(test_dir, output_dir)
